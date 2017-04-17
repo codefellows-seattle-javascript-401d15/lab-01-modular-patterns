9be7add7d1ff2a72f07830393479d22aae08d075
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-04-17 16:57:40 -0700

    added files

---
+++ lab-lindsay/test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+const greet = require('./lib/greet.js');
+
+describe('greet.js', function() {
+    describe('#greetName', function() {
+      it('should return hello lindsay if lindsay is assigned to name', function() {
+        assert.equal(greet.greetName('lindsay'), 'hello lindsay');
+      });
+      it('should return a valid string', function() {
+        assert.equal(typeOf greet.greetName(lindsay), 'string');
+    });
+})

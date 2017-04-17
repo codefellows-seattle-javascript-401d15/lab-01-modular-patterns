39202f7b141f2fc5aca2d66ab3bfbc840eccfbf4
Abigail White <abigailwhite213@gmail.com>
2017-04-17 16:27:27 -0700

    initial structural setup

---
+++ lab-abigail/test/greet-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const assert = require('assert');
+const greeting = require('../lib/greet.js');
+describe('greet.js', function() {
+  describe('#greet', function() {
+    it('should return, hello Abigail', function() {
+      assert.equal(greeting.greet(Abigail), 'hello Abigail');
+    });
+  });
+});

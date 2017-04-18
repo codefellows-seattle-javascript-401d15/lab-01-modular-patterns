43ca0dbcd644c4ff2eb7511e31aa8f6e33902c8e
Ithalis <jt.2012@yahoo.com>
2017-04-18 09:32:41 -0700

    Finished the first stretch goal

--- lab-james/test/greeting-test.js
+++ lab-james/test/greeting-test.js
@@ -6,11 +6,11 @@ const greeting = require('../lib/greeting.js');
 describe('greeting.js', function(){
   describe('#greet', function(){
     it('should return Hello Dave, if given Dave', function(){
-      assert.equal(greeting.greet(Dave), 'Hello Dave');
+      assert.equal(greeting.greet('Dave'), 'Hello Dave');
     });
     it('should return a string', function(){
-      assert.equal(typeof greeting.greet(Dave), 'string');
-      assert.notEqual(typeof greeting.greet(Dave), 'number');
+      assert.equal(typeof greeting.greet('Dave'), 'string');
+      assert.notEqual(typeof greeting.greet('Dave'), 'number');
     });
   });
 });

f066a57912933df856775122f7cbe0e415feecb8
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-04-17 21:31:05 -0700

    testing

--- lab-lindsay/test/test.js
+++ lab-lindsay/test/test.js
@@ -4,11 +4,12 @@ const assert = require('assert');
 const greet = require('./lib/greet.js');
 
 describe('greet.js', function() {
-    describe('#greetName', function() {
-      it('should return hello lindsay if lindsay is assigned to name', function() {
-        assert.equal(greet.greetName('lindsay'), 'hello lindsay');
-      });
-      it('should return a valid string', function() {
-        assert.equal(typeOf greet.greetName(lindsay), 'string');
+  describe('#greetName', function() {
+    it('should return hello lindsay if lindsay is assigned to name', function() {
+      assert.equal(greet.greetName('lindsay'), 'hello lindsay');
     });
-})
+    it('should return a valid string', function() {
+      assert.equal(typeof greet.greetName('lindsay'), 'string');
+    });
+  });
+});

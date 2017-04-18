233209b8ead35765e349e226483d8f44beb4e8dc
Abigail White <abigailwhite213@gmail.com>
2017-04-18 09:02:17 -0700

    second test running

--- lab-abigail/test/greet-test.js
+++ lab-abigail/test/greet-test.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 const greeting = require('../lib/greet.js');
 
+
 describe('greet.js', function() {
   describe('#greet', function() {
     it('should return, hello Abigail', function() {
@@ -10,6 +11,9 @@ describe('greet.js', function() {
     });
     it('should return a string', function() {
       assert.equal(typeof greeting.greet('Abigail'), 'string');
+    });
+    it('should return a string', function() {
+      assert.equal(greeting.greet(process.argv[2]), `hello ${process.argv[2]}`);
     });
   });
 });

3aa6f2c0db8d61d42740dc7aa6524892f0759126
Isak Swearingen <isakswearingen@gmail.com>
2017-04-18 08:54:25 -0700

    min working

--- test/test.js
+++ test/test.js
@@ -7,6 +7,18 @@ describe('module', function(){
   describe('greet', function(){
     it('should say Hello + name', function(){
       assert.equal(hi.greet('Isak'), 'Hello Isak');
-    })
-  })
-})
+    });
+    it('should be a string', function(){
+      assert.equal(typeof hi.greet('Isak'), 'string');
+    });
+    it('should not have any number values', function(){
+      assert.notEqual(hi.greet('Isak'), 'number');
+    });
+    it('should not have any boolean values', function(){
+      assert.notEqual(hi.greet('Isak'), 'boolean');
+    });
+    it('should have at least one character for the name', function(){
+      assert.notEqual(hi.greet('Isak').length, 0);
+    });
+  });
+});

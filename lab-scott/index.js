a928dde0f9b8256885761b97cc26f169990c03d1
Scott Brenden <Scott.b.Brenden@gmail.com>
2017-04-18 09:26:11 -0700

    got base functionality

---
+++ lab-scott/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const greet = require('./lib/greet.js')
+
+// console.log(greet.greeting('scott'));
---
+++ lab-scott/lib/greet.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = exports = {};
+
+exports.greeting = function(name) {
+  return `Hello ${name}`;
+}

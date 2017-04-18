12e5eb95780cb0f4e8eea3837236f524f174f95e
Steven Johnson <stevenjohnson86@gmail.com>
2017-04-18 09:41:34 -0700

    finished stretch goals

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 'use strict';
 
 const greet = require('./lib/greet');
+// const argv = require('argv');
+// const args = argv.options(options).run();
+console.log(greet.greet(process.argv[2]));
--- test/greet.test.js
+++ test/greet.test.js
@@ -3,10 +3,15 @@
 const assert = require('assert');
 const greet = require('../lib/greet');
 
+const name = 'steven'
+
 describe('greet.js', function(){
   describe('#greet', function(){
     it('is a string', function(){
-      assert.equal(typeof greet.greet('steven'), 'string');
+      assert.equal(typeof greet.greet(name), 'string');
+    });
+    it('recieves the argument', function(){
+      assert.strictEqual(greet.greet(name), 'Hello steven');
     });
   });
 });

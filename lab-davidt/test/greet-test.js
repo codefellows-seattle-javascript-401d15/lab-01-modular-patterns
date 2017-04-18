f77f9188ab6c5721ac7daa99e6d032ee0cc34fe0
David Teddy <davidteddy@outlook.com>
2017-04-18 09:42:59 -0700

    did both streach goals

--- lab-davidt/test/greet-test.js
+++ lab-davidt/test/greet-test.js
@@ -2,11 +2,20 @@
 
 const assert = require('assert');
 const greet = require('../lib/greet.js');
-
+var name = 'test';
 describe ('greet.js', function(){
   describe('#greet', function(){
-    it('should show a name, when name is inputed', function(){
+    it('should hello David, when name is inputed', function(){
       assert.equal(typeof greet.greet(), 'string');
+    });
+    it('should not have an empty string', function(){
+      assert.notEqual(greet.greet().length, 0);
+    });
+    it('should not be a number', function(){
+      assert.notEqual(greet.greet(), NaN);
+    });
+    it('should pass in argument', function(){
+      assert.equal(greet.greet(name), 'hello test');
     });
   });
 });

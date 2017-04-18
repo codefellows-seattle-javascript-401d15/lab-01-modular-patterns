00db4dbf8420c815f347e7bed5be25bba2d69a45
Isak Swearingen <isakswearingen@gmail.com>
2017-04-18 10:03:10 -0700

    both stretch goals done

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 'use strict';
-
+// const param = require('../greet.js');
 const hi = require('../lib/lib.js');
 const assert = require('assert');
+
+
 
 describe('module', function(){
   describe('greet', function(){
     it('should say Hello + name', function(){
-      assert.equal(hi.greet('Isak'), 'Hello Isak');
+      assert.equal(hi.greet('Isak'), 'Hello ' + 'Isak');
     });
     it('should be a string', function(){
       assert.equal(typeof hi.greet('Isak'), 'string');
@@ -19,6 +21,17 @@ describe('module', function(){
     });
     it('should have at least one character for the name', function(){
       assert.notEqual(hi.greet('Isak').length, 0);
+    });
+  });
+});
+
+describe('module', function(){
+  describe('name', function(){
+    it('Should be a string', function(){
+      assert.equal(typeof exports.name, 'string');
+    });
+    it('Should not contain a number', function(){
+      assert.notEqual(exports.name, 'number');
     });
   });
 });

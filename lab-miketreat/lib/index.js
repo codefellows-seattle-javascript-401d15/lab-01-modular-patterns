28c3ad93f7cda8309e94bb73a51c03def86c97dc
michaeltreat <michael.leon.treat@gmail.com>
2017-04-17 17:12:31 -0700

    Quick refactor and kill corpsecode/test logs

--- lab-miketreat/lib/index.js
+++ lab-miketreat/lib/index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-console.log('test');
-
-let greeting = require('./greeting.js');
+const greeting = require('./greeting.js');
 
 greeting.greet(process.argv[2]);
-console.log(process.argv);
-console.log(process.argv[2]);

9a65feeb3df335242f9978c060f512887121ba8a
Patrick Sheridan <sheridp2@gmail.com>
2017-04-18 09:26:11 -0700

    stretch goal 1 complete

--- lab-patrick/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const greeting = require('./lib/greeting.js');
-
-console.log(greeting.greet('Patrick'));
--- lab-patrick/lib/greeting.js
+++ lab-patrick/lib/greeting.js
@@ -5,3 +5,5 @@ module.exports = exports = {};
 exports.greet = function(name){
   return`Hello ${name}`;
 };
+
+console.log(exports.greet(process.argv[2]));

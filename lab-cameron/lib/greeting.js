873eeb89bc94f31f0c7ff582e72cf2b9c14e28f1
Cameron Bacon <cameronmbacon@gmail.com>
2017-04-18 09:02:37 -0700

    Can enter a name through command line, but it does not start the test

--- lab-cameron/lib/greeting.js
+++ lab-cameron/lib/greeting.js
@@ -4,6 +4,7 @@ module.exports = exports = {};
 
 // Stretch Goal: typing 'node greeting.js < your name >'
 var param = process.argv[2];
+console.log(`hello ${param}`);
 
 exports.greet = function(name) {
   return `hello ${name}`;
--- lab-cameron/test/greet-test.js
+++ lab-cameron/test/greet-test.js
@@ -6,10 +6,10 @@ const greeting = require('../lib/greeting.js');
 describe('greeting.js', function() {
   describe('#greet', function() {
     it('should say \'hello Cameron\'', function() {
-      assert.equal(greeting.greet(process.argv[2]), 'hello Cameron')
+      assert.equal(greeting.greet('Cameron'), 'hello Cameron');
     });
     it('should return a valid string', function() {
-      assert.equal(typeof greeting.greet(process.argv[2]), 'string')
+      assert.equal(typeof greeting.greet('Cameron'), 'string');
     });
   });
 });

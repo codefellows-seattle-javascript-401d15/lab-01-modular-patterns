576ceb9cf26378ba8eb4b808ab27c59d7d01e8fb
michaeltreat <michael.leon.treat@gmail.com>
2017-04-17 17:10:55 -0700

    got the bonus point working. Now you can pass a value into the terminal and have it put into the greeting function. Eg: typing 'node index.js Mike' will return 'Hello Mike'

--- lab-miketreat/lib/index.js
+++ lab-miketreat/lib/index.js
@@ -4,4 +4,6 @@ console.log('test');
 
 let greeting = require('./greeting.js');
 
-greeting.greet('Mike');
+greeting.greet(process.argv[2]);
+console.log(process.argv);
+console.log(process.argv[2]);

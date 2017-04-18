935635e60abea3e548c9c0bb4ff2196d01b3b1e1
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-04-17 21:25:42 -0700

    add a shot at the stretch goal

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const greet = require('./lib/greet.js');
+
+console.log(process.argv[2]);
--- test/greet-test.js
+++ test/greet-test.js
@@ -4,14 +4,18 @@ const assert = require('assert');
 
 const greet = require('../lib/greet.js');
 
-describe('greet.js', function() {
-  describe('#printGreeting', function() {
-    it('should return a string', function() {
+// var name = 'test';
+
+describe('greet.js', () => {
+  describe('#printGreeting', () => {
+    it('should return a string', () => {
       assert.equal(typeof greet.greet(), 'string');
     });
-    var name = 'David';
-    it('should have a length > 0', function() {
-      assert.notEqual(greet.greet(name).length, 0);
+    it('should have a length > 0', () => {
+      assert.notEqual(greet.greet().length, 0);
+    });
+    it('should not return NaN', () => {
+      assert.notEqual(greet.greet(), NaN);
     });
   });
 });

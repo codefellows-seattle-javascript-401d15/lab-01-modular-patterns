7143754557e9d103a87179430584168d80ccd139
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-04-17 21:32:20 -0700

    changed path

--- lab-lindsay/test/test.js
+++ lab-lindsay/test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const greet = require('./lib/greet.js');
+const greet = require('../lib/greet.js');
 
 describe('greet.js', function() {
   describe('#greetName', function() {

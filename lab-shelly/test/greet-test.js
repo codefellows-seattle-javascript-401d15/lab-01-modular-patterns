ced3675a436b23d38ba898b615d89c43d2420dfe
Shelly Tang <shellytang@gmail.com>
2017-04-18 20:22:10 -0700

    updated for bonus test

--- lab-shelly/test/greet-test.js
+++ lab-shelly/test/greet-test.js
@@ -6,10 +6,13 @@ const greeting = require('../lib/greet.js');
 describe('greet.js', function() {
   describe('#greet', function(){
     it('should return hello with name', function(){
-      assert.equal(greeting.greet('shelly'), 'hello shelly' );
+      assert.equal(greeting.greet('shelly'), 'hello shelly');
     });
     it('should return a string', function() {
       assert.equal(typeof greeting.greet('shelly'), 'string');
+    });
+    it('should pass in the name', function() {
+      'hello process.argv[2]';
     });
   });
 });
